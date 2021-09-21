a9b68c54ed18ab1aa3d52adb77f68e2af526b3f8
Akshay Mishra <akshay@syscloud.org>
2021-09-21 14:08:47 +0530

    some changes done

---
+++ src/components/CustomDialogBox.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {Dialog, DialogTitle,DialogContent,DialogContentText,DialogActions, Button} from "@material-ui/core";
+
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function CustomDialogBox({open, handleClose, submit}) {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={submit}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default CustomDialogBox;
--- src/views/SelectPlayersPage.js
+++ src/views/SelectPlayersPage.js
@@ -1,13 +1,14 @@
-import { Grid, Button } from "@material-ui/core";
+import {Grid, Button } from "@material-ui/core";
 import Avatar from "@mui/material/Avatar";
 import React, { useState, useEffect } from "react";
 import NavigationHeader from "../components/NavigationHeader";
+import CustomDialogBox from "../components/CustomDialogBox";
 import { makeStyles } from "@material-ui/core/styles";
 import ExtractInitials from "../utils/ExtractInitials";
 import { CheckCircle } from "@material-ui/icons";
 import { red } from "@mui/material/colors";
 import axios from "axios";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const useStyles = makeStyles((theme) => ({
@@ -42,12 +43,22 @@ const useStyles = makeStyles((theme) => ({
     width: "300px",
     [theme.breakpoints.down("sm")]: {
       marginRight: theme.spacing(1),
-      width : "230px"
+      width: "230px",
     },
     "&:hover": {
       backgroundColor: red[500],
       color: "white",
     },
+  },
+  submitButton: {
+    marginTop : "20px",
+    marginBottom: "20px",
+    width: "80%",
+    marginLeft : "10%",
+    marginRight: "10%",
+    borderRadius: "10px",
+    backgroundColor: red[500],
+    color: "white",
   },
 }));
 
@@ -55,19 +66,59 @@ function SelectPlayersPage() {
   const classes = useStyles();
   const [initials, setInitials] = useState([]);
   const [playersInfo, setPlayersInfo] = useState([]);
+  const [playersChosen, setPlayersChosen] = useState([]);
+  const [selectedPlayers, setSelectedPlayers] = useState([]);
 
-  const selectPlayer = (index) => {
-    const new_players = [...playersInfo];
-    if (new_players[index]["selected"]) new_players[index]["selected"] = false;
-    else new_players[index]["selected"] = true;
+  const [open, setOpen] = React.useState(false);
 
-    setPlayersInfo(new_players);
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const selectPlayer = (playerId) => {
+    if (playersChosen.length >= 3 && !playersChosen.includes(playerId))
+      alert("Only 3 players can be chosen");
+    else if (playersChosen.includes(playerId)) {
+      const index = playersChosen.indexOf(playerId);
+      if (index > -1) {
+        let new_players_chosen = [...playersChosen];
+        new_players_chosen.splice(index, 1);
+        setPlayersChosen(new_players_chosen);
+      }
+    } else {
+      let new_players_chosen = [...playersChosen, playerId];
+      setPlayersChosen(new_players_chosen);
+    }
+  };
+
+  const submitSelectedPlayers = () => {
+    axios.post(`http://localhost:4000/players/selectPlayers`,
+    {
+      username: "Akshay Mishra",
+      date: null,
+      players : playersChosen
+    })
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
+
   useEffect(() => {
+    let username = "Akshay Mishra";
     axios
-      .get(`http://172.105.37.155:4000/players/getPlayers`)
+      .get(`http://localhost:4000/players/getPlayers`)
       .then((result) => setPlayersInfo(result.data.all_players))
+      .catch((err) => console.log(err));
+
+    axios
+      .get(`http://localhost:4000/players/getSelectedPlayers/${username}`)
+      .then((result) => {
+        setSelectedPlayers(result.data.selected_players);
+      })
       .catch((err) => console.log(err));
 
     let new_initials_Arr = [];
@@ -76,7 +127,7 @@ function SelectPlayersPage() {
       new_initials_Arr.push(initials);
     });
     setInitials(new_initials_Arr);
-  }, [playersInfo]);
+  }, []);
 
   return (
     <div>
@@ -107,11 +158,15 @@ function SelectPlayersPage() {
                     <span>{player.favourite_team}</span>
                   </span>
                   <Button
-                    onClick={() => selectPlayer(index)}
                     variant="outlined"
                     className={classes.selectButton}
+                    onClick={() => selectPlayer(player.id)}
                   >
-                    {player.selected ? <CheckCircle /> : <span>Select</span>}
+                    {playersChosen.includes(player.id) ? (
+                      <CheckCircle />
+                    ) : (
+                      <span>Select</span>
+                    )}
                   </Button>
                 </div>
               </div>
@@ -119,6 +174,16 @@ function SelectPlayersPage() {
           </>
         ))}
       </Grid>
+      {playersChosen.length >= 1 && (
+        <Grid container>
+          <Grid item xs={12}>
+            <Button className={classes.submitButton} variant="contained" onClick={handleClickOpen}>
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      )}
+      <CustomDialogBox open={open} handleClose={handleClose} submit={submitSelectedPlayers}/>
     </div>
   );
 }
